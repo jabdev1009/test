f0c1fb2477d7dbeeac0590d25a1c36414a3172aa
이재빈 <woqls100@daum.net>
2025-10-28 17:28:41 +0900

    282828

--- src/main/java/com/ssafy/test/snapshot/service/ChunkInfo.java
+++ src/main/java/com/ssafy/test/snapshot/service/ChunkInfo.java
@@ -11,7 +11,8 @@ public record ChunkInfo(String worldName, int lod, int x, int y, int z) {
             int z = extractInt(chunkKey, ":z");
             return new ChunkInfo(worldName, lod, x, y, z);
         } catch (Exception e) {
-            throw new IllegalArgumentException("잘못된 chunkKey: " + chunkKey, e);
+            // TODO: CustomException
+            return null;
         }
     }
 
@@ -19,7 +20,8 @@ public record ChunkInfo(String worldName, int lod, int x, int y, int z) {
         int start = key.indexOf("{world:") + 7;
         int end = key.indexOf("}", start);
         if (start < 7 || end < 0) {
-            throw new IllegalArgumentException("WorldName 추출 실패: " + key);
+            // TODO: CustomException
+            return null;
         }
         return key.substring(start, end);
     }
@@ -27,7 +29,8 @@ public record ChunkInfo(String worldName, int lod, int x, int y, int z) {
     private static int extractInt(String key, String prefix) {
         int start = key.indexOf(prefix);
         if (start < 0) {
-            throw new IllegalArgumentException("Prefix '" + prefix + "' 없음: " + key);
+            // TODO: CustomException
+            return 0;
         }
         start += prefix.length();
         int end = key.indexOf(":", start);
